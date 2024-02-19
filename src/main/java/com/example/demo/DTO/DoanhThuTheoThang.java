f1b2bfeb9a25d88d45070c9617b2adf94c49b624
anhnlt0543 <anhnltph28514@fpt.edu.vn>
2024-02-19 14:24:01 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/DTO/DoanhThuTheoThang.java
@@ -0,0 +1,17 @@
+package com.example.demo.DTO;
+
+import java.math.BigDecimal;
+
+public interface DoanhThuTheoThang {
+
+    Integer getThang();
+    Integer getNam();
+    Integer getSoLuongSP();
+    BigDecimal getDoanhThuThucTe();
+    BigDecimal getTienDoiTra();
+    BigDecimal getPhiShip();
+    BigDecimal getDoanhThuChuaKhuyenMai();
+    String getTen();
+
+}
+
